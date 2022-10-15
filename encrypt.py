127bd6a1f6bc3987c3b90e61e9b21a993471c921
Your Namesifer <olegajun@gmail.com>
2022-10-15 11:33:54 +0300

    Modified

--- encrypt.py
+++ encrypt.py
@@ -1,91 +1,134 @@
 import os
 
 
-def encrypt(mode):
+def prepare_key(key, encrypt_or_decrypt):
 
-    if mode.lower() == 'image':
-        print('')
-        path = input(r'Enter path of Image : ')
-        print('')
-        print('Note: Max lenght of a key is 2 digits')
-        try:
-            key = int(input('Enter Key for encryption/decryption of Image : '))
-        except:
-            print('\nKey must be a number. Please try again. Example: 52')
-            return
-        print('\nThe path of file : ', path)
-        print('\nKey for encryption : ', key)
+    # The key is split into a list of numbers by pairs =========================
+    key = str(key)
+    keys_str = [key[i:i+2] for i in range(0, len(key), 2)]
 
-        try:
-            fin = open(path, 'rb')
-            image = fin.read()
-            fin.close()
-        except:
-            print(
-                '\nBad image location input. Please try again. Example: C:\\User\\John\\Desktop\\image.jpg')
-            return
+    # Check if it's encryption or decryption ===================================
+    # All pairs of keys should have a pair.
+    for i in keys_str:
+        if len(i) < 2:
+            i = i + '0'
 
-        image = bytearray(image)
+    keys = [int(i) for i in keys_str]
 
-        for index, values in enumerate(image):
-            image[index] = values ^ key
+    # Decryption must use the entered key in reverse ===========================
+    if encrypt_or_decrypt == 'dec':
+        keys.reverse()
+        return (keys)
+    elif encrypt_or_decrypt == 'enc':
+        return (keys)
+    else:
+        print('Wrong input. Please specify "enc" for encryption and "dec" for decryption')
+        return
 
-        fin = open(path, 'wb')
-        fin.write(image)
-        fin.close()
 
-        print('\n[ Encryption\Decryption is Done! ]')
+def check_path(path):
 
-    elif mode.lower() == 'folder':
-        print('')
-        folder_path = input(r'Enter path of folder : ')
-        print('')
-        print('Note: Max lenght of a key is 2 digits')
-        try:
-            key = int(input('Enter Key for encryption/decryption : '))
-        except:
-            print('\nKey must be a number. Please try again')
-            return
+    if '.' in path:
+        return 'file'
+    else:
+        return 'folder'
 
-        print('\nThe path of file : ', folder_path)
-        print('\nKey for encryption : ', key)
 
-        try:
-            dir_list = os.listdir(folder_path)
-        except:
-            print(
-                '\nBad image location input. Please try again. Example: C:\\User\\John\\Desktop\\image.jpg')
-            return
+def encrypt_file():
+    pass
 
-        count = 1
-        for image in dir_list:
-            if dir_list[-1] != '\\':
-                path = folder_path + '\\' + image
-            else:
-                path = folder_path + image
 
-            fin = open(path, 'rb')
-            image = fin.read()
-            fin.close()
+def encrypt(encrypt_or_decrypt):
+
+    print('')
+    path = input(r'Enter path of Image or Folder : ')
+    print('')
+
+    try:
+        key = int(input('Enter Key for encryption/decryption of Image : '))
+    except:
+        print('\nKey must be a number. Please try again. Example: 52')
+        return
+
+    print('\nThe path of file : ', path)
+    print('\nKey for encryption : ', key)
+
+    try:
+        keys = prepare_key(key, encrypt_or_decrypt)
+    except:
+        print('Wrong input. Please specify "enc" for encryption and "dec" for decryption')
+        return
+
+    mode = check_path(path)
+
+    print('\nThe path of file : ', path)
+    print('\nKey for encryption : ', key)
+
+    if mode == 'file':
+
+        pass_count = 1
+        for i in keys:
+            try:
+                fin = open(path, 'rb')
+                image = fin.read()
+                fin.close()
+            except:
+                print(
+                    '\nBad path input. Please try again. Example: C:\\User\\John\\Desktop\\image.jpg')
+                return
 
             image = bytearray(image)
 
             for index, values in enumerate(image):
-                image[index] = values ^ key
+                image[index] = values ^ i
 
             fin = open(path, 'wb')
             fin.write(image)
             fin.close()
 
-            print(f'\n[ {str(count)} Encryption\Decryption Done!]')
+            print(f'\n[ Pass [{pass_count}] Encryption\Decryption is Done! ]')
+            pass_count += 1
 
-            count += 1
-    else:
-        print('Wrong mode. Please try again (image/folder)')
+    elif mode == 'folder':
+
+        pass_count = 1
+        for i in keys:
+            try:
+                dir_list = os.listdir(path)
+            except:
+                print(
+                    '\nBad path input. Please try again. Example: C:\\User\\John\\Desktop\\image.jpg')
+                return
+
+            count = 1
+            for image in dir_list:
+                if dir_list[-1] != '\\':
+                    img_path = path + '\\' + image
+                else:
+                    img_path = path + image
+
+                fin = open(img_path, 'rb')
+                image = fin.read()
+                fin.close()
+
+                image = bytearray(image)
+
+                for index, values in enumerate(image):
+                    image[index] = values ^ i
+
+                fin = open(img_path, 'wb')
+                fin.write(image)
+                fin.close()
+
+                print(
+                    f'\n[ Pass [{pass_count}] Encryption\Decryption on image #{count} Done!]')
+
+                img_path = ''
+                count += 1
 
 
 if __name__ == '__main__':
 
-    mode = input('Select mode [folder/image]: ')
+    encrypt_or_decrypt = input('Encrypt or Decrypt? [enc/dec]: ')
 
-    encrypt(mode)
+    encrypt(encrypt_or_decrypt)
